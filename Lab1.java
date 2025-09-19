0ca05c1f4750757eca309a0436547e6111924fdb
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-09-19 07:29:56 -0500

    test file

--- Lab1.java
+++ Lab1.java
@@ -2,6 +2,8 @@
 
 public class Lab1 {
     public static void main(String[] args) {
-        
+        String s = "Java is lots of fun!";
+        int x = s.indexOf("a");
+        System.out.print(x);
     }
 }
