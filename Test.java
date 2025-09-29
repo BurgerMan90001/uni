9cd8ac597cf2cfa984ef6028f659e57f1df38c4e
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-09-29 07:11:33 -0500

    commit

---
+++ Test.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        String test = "1111";
+
+        System.out.println("wwwww");
+        Scanner scnr = new Scanner(System.in);
+
+        int num = Integer.parseInt("dddd123");
+        System.out.println(num);
+
+        scnr.close();
+
+        // System.out.printf("%s ", "asdasd");
+    }
+}
