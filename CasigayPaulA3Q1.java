1e30f3e70e8dcbbd22e4c6e538e8df782cc47d44
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-10-17 10:16:29 -0500

    rpc

---
+++ CasigayPaulA3Q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CasigayPaulA3Q1 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        int wallPosition = (int) Math.random();
+
+    }
+
+    static void shootCannon() {
+
+    }
+}
---
+++ Rock.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class Rock {
+    static final String ROCK = "Rock";
+    static final String SCISSORS = "Scissors";
+    static final String PAPER = "Paper";
+
+    static final String PLAYER_WIN = "Player wins!";
+    static final String COMPUTER_WIN = "Computer wins!";
+    static final String DRAW = "Draw";
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        String playerMove = playerMove(scnr);
+        String computerMove = computerMove();
+
+        String gameState = getGameState(playerMove, computerMove);
+
+        System.out.println(gameState);
+
+    }
+
+    static void testSuite() {
+
+    }
+
+    static String getGameState(String playerMove, String computerMove) {
+        String gameState = "";
+        if (playerMove.equals(computerMove)) {
+            gameState = DRAW;
+        }
+
+        else if (playerMove.equals(ROCK) && computerMove.equals(SCISSORS)) {
+            gameState = PLAYER_WIN;
+        } else if (playerMove.equals(ROCK) && computerMove.equals(PAPER)) {
+            gameState = COMPUTER_WIN;
+        }
+
+        else if (playerMove.equals(PAPER) && computerMove.equals(SCISSORS)) {
+            gameState = COMPUTER_WIN;
+        } else if (playerMove.equals(PAPER) && computerMove.equals(ROCK)) {
+            gameState = PLAYER_WIN;
+        }
+
+        else if (playerMove.equals(SCISSORS) && computerMove.equals(PAPER)) {
+            gameState = PLAYER_WIN;
+        } else if (playerMove.equals(SCISSORS) && computerMove.equals(ROCK)) {
+            gameState = COMPUTER_WIN;
+        }
+        return gameState;
+    }
+
+    static String playerMove(Scanner scnr) {
+        System.out.print("> Player move (0 Rock, 1 Paper, 2 Scissors): ");
+        int playerMove = scnr.nextInt();
+
+        String moveName = getMoveName(playerMove);
+
+        if (moveName.equals("Invalid move")) {
+            System.out.println("Invalid move");
+            playerMove(scnr);
+        }
+        System.out.println("> Player chooses: " + playerMove);
+
+        return moveName;
+    }
+
+    static String computerMove() {
+        int comuterMove = (int) (Math.random() * 2);
+        String moveName = getMoveName(comuterMove);
+
+        System.out.println("> Computer chooses: " + moveName);
+
+        return moveName;
+    }
+
+    static String getMoveName(int moveNumber) {
+        String moveName = "";
+        switch (moveNumber) {
+            case 0:
+                moveName = ROCK;
+                break;
+            case 1:
+                moveName = PAPER;
+                break;
+            case 2:
+                moveName = SCISSORS;
+                break;
+            default:
+                moveName = "Invalid move";
+                break;
+        }
+        return moveName;
+    }
+}
