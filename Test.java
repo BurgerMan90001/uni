66edf14751d438eb2c0120b09fddc40c93d5ee3c
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-10-17 08:10:12 -0500

    tests

--- Test.java
+++ Test.java
@@ -3,18 +3,16 @@ import java.util.Scanner;
 class Test {
     public static void main(String[] args) {
         // test2();
-        String test1 = "hello";
-        String ye = "o";
-        String test2 = "hell" + ye;
+        for (int i = 0; i < 5; i++) {
 
-        System.out.println(test1 == test2);
+        }
     }
-
     static void q4() {
         int test = 3 / (2 + 1) % 2;
         int test2 = 3 % 2 + 1 % 2;
         double test3 = (4.5 / 2) % 3 - (5 / 2.0);
         double test4 = 5 / 2 % 1.5 - 10 / -2.0;
+
         System.out.println(test);
         System.out.println(test2);
         System.out.println(test3);
